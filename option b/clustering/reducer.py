7f3dedae5d3482505cd6dc629bd156f457bf4381
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2024-05-15 23:31:22 +0530

    Update reducer.py

--- option b/clustering/reducer.py
+++ option b/clustering/reducer.py
@@ -10,5 +10,4 @@ for data in sys.stdin:
     all.append([centroid, x, y])
 # print(all)
 centroid_new = pd.DataFrame(all).groupby(0).mean()
-for _, row in centroid_new.iterrows():
-    print("New Centroid (x,y): ", row[1], row[2])
+print(centroid_new)
