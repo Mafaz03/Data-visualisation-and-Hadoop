e19ba89b49d33353b09bc9df386a5b4236cebe7a
Mafaz03 <mohdmafaz200303@gmail.com>
2024-05-15 22:07:34 +0530

    bugs

--- option b/classification/reducer.py
+++ option b/classification/reducer.py
@@ -7,8 +7,11 @@ all = []
 for line in sys.stdin:
     line = line.strip()
     state, cause, total = line.split('\t')
+    total = int(total)
     all.append([cause + " : " + state, total])
 
 df = pd.DataFrame(all)
 result = df.groupby(0).sum().reset_index()
+
+pd.set_option('display.max_rows', None)
 print(result)
