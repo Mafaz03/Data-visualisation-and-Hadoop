a9a42ef6c9f4e85ec965510addc7b462803ff269
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2024-05-16 10:45:24 +0530

    Update reducer.py

--- option b/classification/reducer.py
+++ option b/classification/reducer.py
@@ -1,6 +1,9 @@
 import sys
 import pandas as pd
 
+#If the pandas not installed
+#pip install pandas
+
 cause_total = {}
 all = []
 #Partitoner
@@ -16,21 +19,4 @@ result = df.groupby(0).sum().reset_index()
 pd.set_option('display.max_rows', None)
 print(result)
 
-#If the pandas not installed
-# import sys
 
-# all = []
-
-# for lines in sys.stdin:
-#     state,cause,death = lines.strip().split("\t")
-#     all.append([(state + ' : ' + str(cause)),death])
-
-# d = {}
-# for i in all:
-#     if i[0] not in all:
-#         d[i[0]] = i[1]
-#     else:
-#         d[i[0]] += i[1]
-# idx = d.keys()
-# for i in idx:
-#     print(i,d[i])
